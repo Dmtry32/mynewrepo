c22b4d66f3697a884a2b24382d3093ff5ca5637c
anas moussaoui <mogat132@gmail.com>
2023-01-08 21:51:50 +0100

    affiche age

--- python.py
+++
@@ -1,4 +0,0 @@
-nom = input ('quelle est ton nom')
-age = input ('quel est ton age')
-
-print (bonjour {} tu as {} ans et bienvenu a luniversite).format(nom,age)
---
+++ pythonex1.py
@@ -0,0 +1,5 @@
+lanne_de_naissance =int( input ("ton annee de naissance : "))
+
+age = 2023 - lanne_de_naissance
+
+print (age)
