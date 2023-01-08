85122368fe6f9ff695ca4294ca04916254e70ac8
anas moussaoui <mogat132@gmail.com>
2023-01-08 21:16:38 +0100

    exerciceinput,format

---
+++ python.py
@@ -0,0 +1,4 @@
+nom = input ('quelle est ton nom')
+age = input ('quel est ton age')
+
+print (bonjour {} tu as {} ans et bienvenu a luniversite).format(nom,age)
---
+++ pythonex.py
@@ -0,0 +1,3 @@
+nom = input ("comment tu t'apelle : ")
+age= input ("t as quel age : ")
+print (f"bonjour {nom} , tu as {age} ans et bienvenue a luniversite ")
